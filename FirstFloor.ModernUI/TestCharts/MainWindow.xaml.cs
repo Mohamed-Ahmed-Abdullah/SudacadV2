e7f196bd05ecc03008b5651bcd0f31d109867bff
Mohamed Ahmed Abdullah <mohamed.1234@hotmail.com>
2015-01-03 23:33:15 +0300

    the charts still don't show up

---
+++ FirstFloor.ModernUI/TestCharts/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TestCharts.ViewModel;
+
+namespace TestCharts
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new TestPageViewModel();
+        }
+        private void ShellView_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            Matrix m = PresentationSource.FromVisual(Application.Current.MainWindow).CompositionTarget.TransformToDevice;
+            double dx = m.M11;
+            double dy = m.M22;
+
+            ScaleTransform s = (ScaleTransform)mainGrid.LayoutTransform;
+            s.ScaleX = 1 / dx;
+            s.ScaleY = 1 / dy;
+        }
+    }
+}
