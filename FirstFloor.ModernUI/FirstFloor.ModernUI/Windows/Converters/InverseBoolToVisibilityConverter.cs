6c5576b958dccf5b3cf534dd831046630f4662af
Mohamed Ahmed Abdullah <mohamed.1234@hotmail.com>
2015-01-06 17:34:32 +0300

    datagrid column visibility

---
+++ FirstFloor.ModernUI/FirstFloor.ModernUI/Windows/Converters/InverseBoolToVisibilityConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace FirstFloor.ModernUI.Windows.Converters
+{
+    /// <summary>
+    /// convert visibility to bool value
+    /// </summary>
+    public class InverseBoolToVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return false;
+
+            if ((Visibility)value == Visibility.Visible)
+                return true;
+
+            return false;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return Visibility.Collapsed;
+
+            if ((bool)value)
+                return Visibility.Visible;
+
+            return Visibility.Collapsed;
+
+        }
+    }
+}
